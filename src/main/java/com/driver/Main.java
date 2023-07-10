999b26880a1e553811084c2844dba5d09e80748c
Durga0808 <bejawada.durgaprasad@gmail.com>
2023-07-11 01:29:55 +0530

    re

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -19,15 +19,14 @@ public class Main {
             return x*y*z;
         }
     }
-    
 
-    public  static void main (String []args)throws IOException{
+
+    public static void main (String []args)throws IOException{
     Product p= new Product();
-    int x=5;
-    int y=6;
-    int a1=p.product(x,y);
+
+    int a1=p.product(5,6);
     System.out.println(a1);
-    int a2=p.product(x,y,8);
+    int a2=p.product(9,10,8);
     System.out.println(a2);
     double d1=p.product(2.0,5.0);
     System.out.println(d1);
